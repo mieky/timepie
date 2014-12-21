f939c56c0a66e0816a2b527135ee78d94ec4373b
Mike Arvela <mike.arvela@futurice.com>
2014-12-21 21:06:28 +0200

    Draw d3 example donut

--- app/src/timepie.ts
+++ app/src/timepie.ts
@@ -1,10 +1,37 @@
 ///<reference path='./node.d.ts' />
 
 var d3 = require("d3");
-console.log("d3", d3);
 
 function greeter(d3 : any) {
     return "Got us some d3 here: " + d3.version;
 }
 
 document.body.innerHTML = greeter(d3);
+
+var data = [1, 1, 2, 3, 5, 8, 13, 21];
+
+var width = 960,
+    height = 500,
+    radius = height / 2 - 10;
+
+var arc = d3.svg.arc()
+    .innerRadius(radius - 40)
+    .outerRadius(radius)
+    .cornerRadius(20);
+
+var pie = d3.layout.pie()
+    .padAngle(.02);
+
+var color = d3.scale.category10();
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+svg.selectAll("path")
+    .data(pie(data))
+    .enter().append("path")
+    .style("fill", function(d, i) { return color(i); })
+    .attr("d", arc);
