0e0da01345499b0e37fd0aa644b0c772904f900b
Mike Arvela <mike.arvela@futurice.com>
2015-01-10 13:03:34 +0200

    Rx'ify non-touch handlers

--- app/src/events.ts
+++ app/src/events.ts
@@ -2,29 +2,36 @@
     Event handlers for touch and non-touch.
 */
 
+var Rx = require("rx");
+
 function initializeTouch(app) {
     document.addEventListener("touchend", app.pause.bind(app));
     window.addEventListener("scroll", function(e) {
         e.preventDefault();
         e.stopPropagation();
-        }, false);
+    }, false);
 
-        app.displayStatus("swipe the time, tap to go!");
+    app.displayStatus("swipe the time, tap to go!");
 }
 
 function initializeNonTouch(app) {
-    document.addEventListener("click", app.pause.bind(app));
-    document.addEventListener("keydown", function(e) {
-        switch (e.keyCode) {
-            case 32: app.pause(); break;
-            case 38: app.adjustMinutes(+1); break; // up
-            case 40: app.adjustMinutes(-1); break; // down
-            case 37: app.adjustSeconds(-1); break; // left
-            case 39: app.adjustSeconds(+1); break; // right
-        }
-        }.bind(app));
+    var clickStream = Rx.Observable.fromEvent(document, "click")
+        .do(app.pause.bind(app))
+        .subscribe();
 
-        app.displayStatus("<em>space</em> plays / pauses<br /><em>arrow keys</em> adjust time");
+    var keyStream = Rx.Observable.fromEvent(document, "keydown")
+        .do((e) => {
+            switch (e.keyCode) {
+                case 32: app.pause(); break;
+                case 38: app.adjustMinutes(+1); break; // up
+                case 40: app.adjustMinutes(-1); break; // down
+                case 37: app.adjustSeconds(-1); break; // left
+                case 39: app.adjustSeconds(+1); break; // right
+            }
+        })
+        .subscribe();
+
+    app.displayStatus("<em>space</em> plays / pauses<br /><em>arrow keys</em> adjust time");
 }
 
 export function initialize(app) {
