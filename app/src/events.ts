138d00dd480794ae07ef3c42e69f31b008e12aaf
Mike Arvela <mike.arvela@futurice.com>
2015-01-11 09:50:19 +0200

    Use swipe to adjust the time on touch

--- app/src/events.ts
+++ app/src/events.ts
@@ -5,13 +5,87 @@
 var Rx = require("rx");
 
 function initializeTouch(app) {
-    document.addEventListener("touchend", app.pause.bind(app));
+
     window.addEventListener("scroll", function(e) {
         e.preventDefault();
         e.stopPropagation();
     }, false);
 
-    app.displayStatus("swipe the time, tap to go!");
+    var tapStream = Rx.Observable.fromEvent(document, "touchstart");
+    var multiTapStream = tapStream
+        .buffer((x) => { return tapStream.throttle(250); })
+        .map((list) => { return list.length; })
+        .filter((x) => { return x === 2; })
+        .subscribe(app.pause.bind(app));
+
+    var touchStream = Rx.Observable.fromEvent(document, "touchstart")
+        .do((e) => {
+            this.dy = 0;
+            this.dx = 0;
+            this.startXy = {
+                x: e.touches[0].pageX,
+                y: e.touches[0].pageY
+            };
+        })
+        .map((e) => {
+            e.stopPropagation();
+
+            return Rx.Observable.fromEvent(document, "touchmove")
+                .takeUntil(Rx.Observable.fromEvent(document, "touchend"));
+        })
+        .switch();
+
+    var swipeVerticalStream = touchStream
+        .filter(() => {
+            return this.dy !== undefined;
+        })
+        .do((e) => {
+            if (Math.abs(e.touches[0].pageY - this.startXy.y) >= 5) {
+                delete this.dx;
+            }
+        })
+        .map((e) => {
+            var prevY = this.prevY || 0;
+            var dir = e.touches[0].pageY > prevY ? -1 : 1;
+
+            // Don't change values until locked on an axis, or if position unchanged
+            if (this.dx !== undefined && this.dy !== undefined || e.touches[0].pageY === prevY) {
+                dir = 0;
+            }
+
+            this.prevY = e.touches[0].pageY;
+            return dir;
+        })
+        .throttleFirst(40) // slow down a bit
+        .do((dir) => { app.adjustMinutes(dir); })
+        .subscribe();
+
+    var swipeHorizontalStream = touchStream
+        .filter(() => {
+            return this.dx !== undefined;
+        })
+        .do((e) => {
+            if (Math.abs(e.touches[0].pageX - this.startXy.x) >= 5) {
+                delete this.dy;
+            }
+        })
+        .map((e) => {
+            var prevX = this.prevX || 0;
+            var dir = e.touches[0].pageX > prevX ? 1 : -1;
+
+            // Don't change values until locked on an axis, or if position unchanged
+            if (this.dx !== undefined && this.dy !== undefined || e.touches[0].pageX === prevX) {
+                dir = 0;
+            }
+
+            this.prevX = e.touches[0].pageX;
+            return dir;
+        })
+        .throttleFirst(20)
+        .do((dir) => { app.adjustSeconds(dir); })
+        .subscribe();
+
+    app.displayStatus("!swipe the time, double-tap to go!");
 }
 
 function initializeNonTouch(app) {
@@ -27,6 +101,13 @@ function initializeNonTouch(app) {
             }
         })
         .subscribe();
+
+    var clickStream = Rx.Observable.fromEvent(document, "mousedown");
+    var multiClickStream = clickStream
+        .buffer((x)   => { return clickStream.throttle(250); })
+        .map((list)   => { return list.length; })
+        .filter((x)   => { return x === 2; })
+        .subscribe(app.pause.bind(app));
 
     var mouseStream = Rx.Observable.fromEvent(document, "mousedown")
         .do((e) => {
