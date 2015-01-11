f945163f8825c89749ca4affb98f2052c89d6c09
Mike Arvela <mike.arvela@futurice.com>
2015-01-11 10:50:04 +0200

    Clean up non-useful mouse time adjustments, change minutes/seconds for arrow keys

--- app/src/events.ts
+++ app/src/events.ts
@@ -5,7 +5,6 @@
 var Rx = require("rx");
 
 function initializeTouch(app) {
-
     window.addEventListener("scroll", function(e) {
         e.preventDefault();
         e.stopPropagation();
@@ -28,6 +27,7 @@ function initializeTouch(app) {
             };
         })
         .map((e) => {
+            e.preventDefault();
             e.stopPropagation();
 
             return Rx.Observable.fromEvent(document, "touchmove")
@@ -89,93 +89,19 @@ function initializeTouch(app) {
 }
 
 function initializeNonTouch(app) {
-
     var keyStream = Rx.Observable.fromEvent(document, "keydown")
         .do((e) => {
             switch (e.keyCode) {
                 case 32: app.pause(); break;
-                case 38: app.adjustMinutes(+1); break; // up
-                case 40: app.adjustMinutes(-1); break; // down
-                case 37: app.adjustSeconds(-1); break; // left
-                case 39: app.adjustSeconds(+1); break; // right
+                case 38: app.adjustSeconds(+1); break; // up
+                case 40: app.adjustSeconds(-1); break; // down
+                case 37: app.adjustMinutes(-1); break; // left
+                case 39: app.adjustMinutes(+1); break; // right
             }
         })
         .subscribe();
 
-    var clickStream = Rx.Observable.fromEvent(document, "mousedown");
-    var multiClickStream = clickStream
-        .buffer((x)   => { return clickStream.throttle(250); })
-        .map((list)   => { return list.length; })
-        .filter((x)   => { return x === 2; })
-        .subscribe(app.pause.bind(app));
-
-    var mouseStream = Rx.Observable.fromEvent(document, "mousedown")
-        .do((e) => {
-            this.dy = 0;
-            this.dx = 0;
-            this.startXy = {
-                x: e.x,
-                y: e.y
-            };
-        })
-        .map((e) => {
-            e.stopPropagation();
-
-            return Rx.Observable.fromEvent(document, "mousemove")
-                .takeUntil(Rx.Observable.fromEvent(document, "mouseup"));
-        })
-        .switch();
-
-    var mouseVerticalStream = mouseStream
-        .filter(() => {
-            return this.dy !== undefined;
-        })
-        .do((e) => {
-            if (Math.abs(e.y - this.startXy.y) >= 5) {
-                delete this.dx;
-            }
-        })
-        .map((e) => {
-            var prevY = this.prevY || 0;
-            var dir = e.y > prevY ? -1 : 1;
-
-            // Don't change values until locked on an axis, or if position unchanged
-            if (this.dx !== undefined && this.dy !== undefined || e.y === prevY) {
-                dir = 0;
-            }
-
-            this.prevY = e.y;
-            return dir;
-        })
-        .throttleFirst(40) // slow down a bit
-        .do((dir) => { app.adjustMinutes(dir); })
-        .subscribe();
-
-    var mouseHorizontalStream = mouseStream
-        .filter(() => {
-            return this.dx !== undefined;
-        })
-        .do((e) => {
-            if (Math.abs(e.x - this.startXy.x) >= 5) {
-                delete this.dy;
-            }
-        })
-        .map((e) => {
-            var prevX = this.prevX || 0;
-
-            var dir = e.x > prevX ? 1 : -1;
-
-            // Don't change values until locked on an axis, or if position unchanged
-            if (this.dx !== undefined && this.dy !== undefined || e.x === prevX) {
-                dir = 0;
-            }
-
-            this.prevX = e.x;
-            return dir;
-        })
-        .throttleFirst(20)
-        .do((dir) => { app.adjustSeconds(dir); })
-        .subscribe();
+    window.addEventListener("dbclick", app.pause.bind(app));
 
     app.displayStatus("<em>space</em> plays / pauses<br /><em>arrow keys</em> adjust time");
 }
